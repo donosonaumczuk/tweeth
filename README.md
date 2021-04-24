b26cd5262ca6310f355d62a6cfc8a6b076442a7b
Alan <adonoso@itba.edu.ar>
2021-04-24 19:34:44 -0300

    Infura websocket connection added (#1)
    * Some dependencies added to package.yaml
    
    * README updated
    
    * Files updated after build
    
    * Websockets template
    
    * Secure websocket connection package added (wuss)
    
    * .gitignore for IntelliJ
    
    * Websocket connection code improved
    
    * Infura Project ID get from environment variable
    
    * Readme upadted with setting envrionment variable instruction
    
    * Code comments removed

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # tweeth
+
+## Build
+
+```
+stack build
+```
+
+## Run
+
+### Setting the Infura Project ID
+In order to avoid exposing the Infura Project ID (kind of API Key), you must set it as `TWEETH_INFURA_PROJECT_ID` environment variable.
+Supposing your Infura Proyect ID is `myT35t1nFu84pr0y3ct1d` ou can do it with the following command:
+ 
+```
+export TWEETH_INFURA_PROJECT_ID=myT35t1nFu84pr0y3ct1d
+```
+
+### Execute run command
+```
+stack exec tweeth-exe
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,44 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+module Main
+    ( main
+    ) where
+
+import           Control.Concurrent  (forkIO)
+import           Control.Monad       (forever, unless)
+import           Control.Monad.Trans (liftIO)
+import           Data.Text           (Text)
+import           Network.Socket      (withSocketsDo)
+import           System.Environment  (getEnv)
+import           Wuss                (runSecureClient)
+import qualified Data.Text           as T
+import qualified Data.Text.IO        as T
+import qualified Network.WebSockets  as WS
+
+app :: WS.ClientApp ()
+app connection = do
+    putStrLn "Connected to Infura Websocket!"
+
+    let putWsIntoStdout = do
+            receivedData <- WS.receiveData connection
+            liftIO $ T.putStrLn receivedData
+
+    threadId <- forkIO $ forever $ do putWsIntoStdout
+
+    let putStdinIntoWs = do
+            line <- T.getLine
+            unless (T.length line == 0) $ WS.sendTextData connection line >> putStdinIntoWs
+
+    let printThreadId = putStrLn ("ThreadId = " ++ show threadId)
+
+    putStdinIntoWs
+    printThreadId
+    WS.sendClose connection ("Connection closed!" :: Text)
 
 main :: IO ()
-main = someFunc
+main = do
+  infuraProjectId <- getEnv "TWEETH_INFURA_PROJECT_ID"
+  let wsPort = 443
+  let wsInfuraUrl = "mainnet.infura.io"
+  let wsInfuraPath = "/ws/v3/" ++ infuraProjectId
+  withSocketsDo $ runSecureClient wsInfuraUrl wsPort wsInfuraPath app
