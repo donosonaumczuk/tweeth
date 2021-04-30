23e919f4dd791004121e76b7016edd420b52ffeb
Alan <adonoso@itba.edu.ar>
2021-04-29 22:03:01 -0300

    Infura ETH types & Tweet format added (#3)
    * EthTypes and Aeson added
    
    * Response parsed and human-friendly tweet format implemented
    
    * Only DAI Transfer event subscription
    
    * Token amount format improved
    
    * README improved
    
    * Code improved

--- README.md
+++ README.md
@@ -1,8 +1,37 @@
 # tweeth
 
-Twitter bot that connects to Infura websockets to listen some Ethereum Smart Contracts events and tweet them.
+[Twitter](https://twitter.com/) bot that connects to [Infura](https://infura.io/) websockets to listen to some [Ethereum](https://ethereum.org/) Smart Contract events and tweets them.
 
-Built in Haskell for Functional Programming class @ITBA.
+Built in [Haskell](https://www.haskell.org/) for Functional Programming class at ITBA.
+
+Read its tweets at [@tweeth_bot](https://twitter.com/tweeth_bot)! ;)
+
+## Setup
+
+### Setting Infura Project ID
+In order to avoid exposing the Infura Project ID (kind of API Key), you must set it as `TWEETH_INFURA_PROJECT_ID` environment variable with the following command:
+ 
+```
+export TWEETH_INFURA_PROJECT_ID="YOUR-INFURA-PROJECT-ID"
+```
+
+### Setting Twitter OAuth
+
+In order to avoid exposing the Twitter OAuth keys, you must set them as four environment variables: `OAUTH_CONSUMER_KEY`, `OAUTH_CONSUMER_SECRET`, `OAUTH_ACCESS_TOKEN` and `OAUTH_ACCESS_SECRET`.
+
+After enabling your Twitter Developer Account, you can find or generate all of them in the [Twitter Developer Portal](https://developer.twitter.com/en/portal/projects-and-apps).
+```
+export OAUTH_CONSUMER_KEY="YOUR-TWITTER-API-KEY"
+```
+```
+export OAUTH_CONSUMER_SECRET="YOUR-TWITTER-API-SECRET-KEY"
+```
+```
+export OAUTH_ACCESS_TOKEN="YOUR-TWITTER-ACCESS-TOKEN"
+```
+```
+export OAUTH_ACCESS_SECRET="YOUR-TWITTER-SECRET-ACCESS-TOKEN"
+```
 
 ## Build
 
@@ -12,15 +41,6 @@ stack build
 
 ## Run
 
-### Setting the Infura Project ID
-In order to avoid exposing the Infura Project ID (kind of API Key), you must set it as `TWEETH_INFURA_PROJECT_ID` environment variable.
-Supposing your Infura Proyect ID is `myT35t1nFu84pr0y3ct1d` you can do it with the following command:
- 
-```
-export TWEETH_INFURA_PROJECT_ID=myT35t1nFu84pr0y3ct1d
-```
-
-### Execute run command
 ```
 stack exec tweeth-exe
 ```
--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,23 @@ module Main
     ( main
     ) where
 
+import           EthTypes
 import           Web.Twitter.Conduit
 import           TwitterUtils
-import           Control.Concurrent  (forkIO)
-import           Control.Monad       (forever, unless)
-import           Control.Monad.Trans (liftIO)
-import           Data.Text           (Text)
-import           Network.Socket      (withSocketsDo)
-import           System.Environment  (getEnv)
-import           Wuss                (runSecureClient)
-import qualified Data.Text           as T
-import qualified Data.Text.IO        as T
-import qualified Network.WebSockets  as WS
+import           Control.Concurrent      (forkIO)
+import           Control.Monad           (forever, unless)
+import           Control.Monad.Trans     (liftIO)
+import           Data.Text               (Text)
+import           Network.Socket          (withSocketsDo)
+import           System.Environment      (getEnv)
+import           Wuss                    (runSecureClient)
+import           Data.Aeson              (decode, FromJSON)
+import           Data.Text.Encoding      (encodeUtf8Builder)
+import           Data.ByteString.Builder (toLazyByteString)
+import           Data.Char               (isAsciiUpper, isAsciiLower)
+import qualified Data.Text                as T
+import qualified Data.Text.IO             as T
+import qualified Network.WebSockets       as WS
 
 app :: WS.ClientApp ()
 app connection = do
@@ -33,25 +38,72 @@ loopUnless loopConditionOverText = do
 tweetWsData :: WS.Connection -> IO ()
 tweetWsData connection = do
     wsReceivedData <- WS.receiveData connection
-    putStrLn "\n\n--------------------"
-    liftIO $ T.putStrLn wsReceivedData
-    tweet wsReceivedData
+    putStrLn "\n\n---------------------------------------"
+    liftIO $ putStrLn ("\n\n-- Websocket event:\n" ++ show wsReceivedData)
+    maybeTweetResultData (responseToStatus wsReceivedData)
 
 sendEthSubscribeRequest :: WS.Connection -> IO ()
 sendEthSubscribeRequest connection = do
     let requestBodyText = T.pack "{\"jsonrpc\":\"2.0\", \"id\": 2, \"method\": \"eth_subscribe\",\
-        \\"params\": [\"logs\", {\"address\": \"0x6b175474e89094c44da98b954eedeac495271d0f\"}]}"
+        \\"params\": [\"logs\", {\"address\": \"0x6b175474e89094c44da98b954eedeac495271d0f\",\
+        \\"topics\": [\"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef\"]}]}"
     WS.sendTextData connection requestBodyText
 
-tweet :: Text -> IO ()
-tweet status = do
-    let maxTweetChars = 280
-    let limitedStatus = T.take maxTweetChars status
-    T.putStrLn $ "\n\nTweet to post: " <> limitedStatus
+decode'' :: FromJSON a => Text -> Maybe a
+decode'' = decode . toLazyByteString . encodeUtf8Builder
+
+responseToStatus :: Text -> Maybe Text
+responseToStatus text = do
+    ethSubResponse <- decode'' text :: Maybe EthSubscription
+    getStatusFromEthSub ethSubResponse
+
+maybeTweetResultData :: Maybe Text -> IO ()
+maybeTweetResultData (Just status) = do
+    logStatusIfPresent status
     twInfo <- getTWInfoFromEnv
     manager <- newManager tlsManagerSettings
-    response <- call twInfo manager $ statusesUpdate limitedStatus
-    putStrLn ("\n\nTwitter API Response: " ++ show response)
+    response <- call twInfo manager $ statusesUpdate status
+    putStrLn $ "\n\n-- Twitter API Response:\n" ++ show response
+maybeTweetResultData Nothing = putStrLn "\n\n-- Nothing to tweet this time :)"
+
+getStatusFromEthSub :: EthSubscription -> Maybe Text
+getStatusFromEthSub (EthSubscription _ _ (EthParams _ (EthResult _ _ _ txHash _ _ _ txData txTopics))) =
+    return $ T.pack ("\
+        \[New DAI Transfer]\n\
+        \- FROM: " ++ (T.unpack . formatTextTopicAsEthAddress) (txTopics!!1) ++ "\n\
+        \- TO: " ++ (T.unpack . formatTextTopicAsEthAddress) (txTopics!!2) ++ "\n\
+        \- AMOUNT: " ++ formatAmount tokenDecimals (show (hexStringToInteger (txDataWithout0x txData))) ++ " DAI\n\
+        \etherscan.io/tx/" ++ T.unpack txHash
+    )
+
+formatTextTopicAsEthAddress :: Text -> Text
+formatTextTopicAsEthAddress text = T.append (T.pack "0x") (T.drop 26 text)
+
+logStatusIfPresent :: Text -> IO ()
+logStatusIfPresent status = T.putStrLn $ "\n\n-- Tweet status to post:\n" <> status
+
+tokenDecimals :: Int
+tokenDecimals = 18
+
+formatAmount :: Int -> String -> String
+formatAmount dec str = (\dec'' str'' -> format dec'' str'' (length str'')) dec (appendZerosToReachDecimals dec str)
+    where format dec'' str'' strLen'' = take (strLen'' - dec'') str'' ++ "." ++ drop (strLen'' - dec'') str''
+
+appendZerosToReachDecimals :: Int -> String -> String
+appendZerosToReachDecimals dec str =
+    if length str > dec then str
+    else appendZerosToReachDecimals dec ("0" ++ str)
+
+txDataWithout0x :: Text -> String
+txDataWithout0x txData = drop 2 (T.unpack txData)
+
+hexStringToInteger :: String -> Integer
+hexStringToInteger [] = 0
+hexStringToInteger str = fromIntegral z + 16 * hexStringToInteger (init str)
+    where z = let y = last str in
+            if isAsciiUpper y then fromEnum y - 55
+            else if isAsciiLower y then fromEnum y - 87
+            else fromEnum y - 48
 
 main :: IO ()
 main = do
