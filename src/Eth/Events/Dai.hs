13d06e1c8542e5825c8511b3587cef58220d60ce
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-02 18:30:12 -0300

    Contract address and signature hash no more as magic constants

--- src/Eth/Events/Dai.hs
+++ src/Eth/Events/Dai.hs
@@ -10,6 +10,12 @@ import qualified Eth.Utils.Format as F
 daiDecimals :: Int
 daiDecimals = 18
 
+daiTokenContractAddress :: Text
+daiTokenContractAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+
+transferSignatureHash :: Text
+transferSignatureHash = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+
 transfer :: TweetableEthEvent
 transfer = TweetableEthEvent {
     matches = matchesTransferEvent,
@@ -18,8 +24,7 @@ transfer = TweetableEthEvent {
 
 matchesTransferEvent :: EthSubscription -> Bool
 matchesTransferEvent (EthSubscription _ _ (EthParams _ (EthResult _ _ _ _ _ _ contractAddress _ txTopics))) =
-    contractAddress == "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-        && head txTopics == "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+    contractAddress == daiTokenContractAddress && head txTopics == transferSignatureHash
 
 transferEventAsTweet :: EthSubscription -> Text
 transferEventAsTweet (EthSubscription _ _ (EthParams _ (EthResult _ _ _ txHash _ _ _ txData txTopics))) =
--- src/Eth/Events/ProofOfHumanity.hs
+++ src/Eth/Events/ProofOfHumanity.hs
@@ -7,6 +7,12 @@ import           Eth.Types
 import qualified Data.Text        as T
 import qualified Eth.Utils.Format as F
 
+addSubmissionSignatureHash :: Text
+addSubmissionSignatureHash = "0x803727a67d35270dc2c090dc4f9cba1f9818a7200e65c2087eca187851fd6b19"
+
+proofOfHumanityContractAddress :: Text
+proofOfHumanityContractAddress = "0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb"
+
 addSubmission :: TweetableEthEvent
 addSubmission = TweetableEthEvent {
     matches = matchesAddSubmissionEvent,
@@ -15,8 +21,7 @@ addSubmission = TweetableEthEvent {
 
 matchesAddSubmissionEvent :: EthSubscription -> Bool
 matchesAddSubmissionEvent (EthSubscription _ _ (EthParams _ (EthResult _ _ _ _ _ _ contractAddress _ txTopics))) =
-    contractAddress == "0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb"
-        && head txTopics == "0x803727a67d35270dc2c090dc4f9cba1f9818a7200e65c2087eca187851fd6b19"
+    contractAddress == proofOfHumanityContractAddress && head txTopics == addSubmissionSignatureHash
 
 addSubmissionEventAsTweet :: EthSubscription -> Text
 addSubmissionEventAsTweet (EthSubscription _ _ (EthParams _ (EthResult _ _ _ txHash _ _ _ _ txTopics))) =
