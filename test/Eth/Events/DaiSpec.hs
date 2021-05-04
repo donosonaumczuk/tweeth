13e2327eca21d057e6429956a8c583b295e4e9cf
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-03 21:58:53 -0300

    PoH and Dai unit tests added

--- test/Eth/Events/DaiSpec.hs
+++ test/Eth/Events/DaiSpec.hs
@@ -2,36 +2,52 @@
 
 module Eth.Events.DaiSpec (main, spec) where
 
-import Test.Hspec
-import Eth.Types
-import Data.Text (Text, pack)
+import           Eth.Types
+import           Test.Hspec
+import qualified Data.Text.IO as T
 import qualified Eth.Events.Dai as DaiEvents
 
-main :: IO ()
-main = hspec spec
-
 daiTransferSubscription :: EthSubscription
-daiTransferSubscription = EthSubscription "2.0" "eth_subscription" (EthParams "0x1feed340995274b5a3f497fb491dcb4e" 
-    (EthResult False "0x5d" "0x27" "0x2ecd88a2c0ed09d136f175f9843f78c8532de065001be03ed18bf9e4d550d553" 
-    "0x5d8fd3ad85a97b941f3e1f50bb54075d1b18644824a50d39039ebd78b87598fb" "0xbbe061" 
-    "0x6B175474E89094C44Da98b954EedeAC495271d0F" "0x00000000000000000000000000000000000000000000005426c02c9b218f5071" 
+daiTransferSubscription = EthSubscription "2.0" "eth_subscription" (EthParams "0x1feed340995274b5a3f497fb491dcb4e"
+    (EthResult False "0x5d" "0x27" "0x2ecd88a2c0ed09d136f175f9843f78c8532de065001be03ed18bf9e4d550d553"
+    "0x5d8fd3ad85a97b941f3e1f50bb54075d1b18644824a50d39039ebd78b87598fb" "0xbbe061"
+    "0x6B175474E89094C44Da98b954EedeAC495271d0F" "0x00000000000000000000000000000000000000000000005426c02c9b218f5071"
     ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
      "0x0000000000000000000000007515be43d16f871588adc135d58a9c30a71eb34f",
       "0x0000000000000000000000002ad95483ac838e2884563ad278e933fba96bc242"]))
 
-daiTransfer :: TweetableEthEvent
-daiTransfer = DaiEvents.transfer
+subscriptionWithoutTransferTopic :: EthSubscription
+subscriptionWithoutTransferTopic = EthSubscription "2.0" "eth_subscription"
+    (EthParams "0x1feed340995274b5a3f497fb491dcb4e"
+    (EthResult False "0x5d" "0x27" "0x2ecd88a2c0ed09d136f175f9843f78c8532de065001be03ed18bf9e4d550d553"
+    "0x5d8fd3ad85a97b941f3e1f50bb54075d1b18644824a50d39039ebd78b87598fb" "0xbbe061"
+    "0x6B175474E89094C44Da98b954EedeAC495271d0F" "0x00000000000000000000000000000000000000000000005426c02c9b218f5071"
+    ["0x2ecd88a2c0ed09d136f175f9843f78c8532de065001dc135d58a9c30a71e933f",
+     "0x0000000000000000000000007515be43d16f871588adc135d58a9c30a71eb34f",
+      "0x0000000000000000000000002ad95483ac838e2884563ad278e933fba96bc242"]))
 
-readFileAsText :: FilePath -> IO Text
-readFileAsText path = do
-  x <- readFile path
-  return $ pack x
+subscriptionWithoutDaiAddress :: EthSubscription
+subscriptionWithoutDaiAddress = EthSubscription "2.0" "eth_subscription"
+    (EthParams "0x1feed340995274b5a3f497fb491dcb4e"
+    (EthResult False "0x5d" "0x27" "0x2ecd88a2c0ed09d136f175f9843f78c8532de065001be03ed18bf9e4d550d553"
+    "0x5d8fd3ad85a97b941f3e1f50bb54075d1b18644824a50d39039ebd78b87598fb" "0xbbe061"
+    "0x12345474E89094C44Da98b954EedeAC49527FFFF" "0x00000000000000000000000000000000000000000000005426c02c9b218f5071"
+    ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+     "0x0000000000000000000000007515be43d16f871588adc135d58a9c30a71eb34f",
+      "0x0000000000000000000000002ad95483ac838e2884563ad278e933fba96bc242"]))
 
 spec :: Spec
 spec = do
   describe "Eth.Events.Dai tests" $ do
-    it "returns that matches the event" $ do
-      daiTransferSubscription `shouldSatisfy` matches daiTransfer
-    it "returns the expected tweet text" $ do
-      expectedDaiTransferTweet <- readFileAsText "test/Files/daiTransfer.txt"
-      asTweet daiTransfer daiTransferSubscription `shouldBe` expectedDaiTransferTweet
+    it "given dai transfer subscription when calling matches on transfer event then return true" $ do
+      daiTransferSubscription `shouldSatisfy` matches DaiEvents.transfer
+    it "given dai subscription without transfer topic when calling matches on transfer event then return false" $ do
+      subscriptionWithoutTransferTopic `shouldNotSatisfy` matches DaiEvents.transfer
+    it "given subscription without dai contract address when calling matches on transfer event then return false" $ do
+      subscriptionWithoutDaiAddress `shouldNotSatisfy` matches DaiEvents.transfer
+    it "given valid dai transfer subscription when calling asTweet on transfer event then return the expected text" $ do
+      expectedDaiTransferTweet <- T.readFile "test/Files/Eth/Events/Dai/expectedTransferTweet.txt"
+      asTweet DaiEvents.transfer daiTransferSubscription `shouldBe` expectedDaiTransferTweet
+
+main :: IO ()
+main = hspec spec
---
+++ test/Eth/Events/ProofOfHumanitySpec.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Eth.Events.ProofOfHumanitySpec (main, spec) where
+
+import           Eth.Types
+import           Test.Hspec
+import qualified Data.Text.IO as T
+import qualified Eth.Events.ProofOfHumanity as PohEvents
+
+pohAddSubmission :: EthSubscription
+pohAddSubmission = EthSubscription "2.0" "eth_subscription" (EthParams "0x1feed340b3559156f7e39013b6a1edd"
+    (EthResult False "0x134" "0x77" "0xc1014379a142881a1859f0faa7bd5ea5c9396b65f19c8746c055f0b7c3f693e7"
+    "0xd2eb6976b405c2c25f0f849858e5f89629a29f0816d73dfd391d5c891a10468b" "0xbca937"
+    "0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb" "0x0000000000000000000000000000000000000000000000000000000000000000"
+    ["0x803727a67d35270dc2c090dc4f9cba1f9818a7200e65c2087eca187851fd6b19",
+     "0x000000000000000000000000bbeb5e0adc0818ac8677b2f78387a8a2e22e8d16"]))
+
+subscriptionWithoutAddSubmissionTopic :: EthSubscription
+subscriptionWithoutAddSubmissionTopic = EthSubscription "2.0" "eth_subscription" 
+    (EthParams "0x1feed340b3559156f7e39013b6a1edd"
+    (EthResult False "0x134" "0x77" "0xc1014379a142881a1859f0faa7bd5ea5c9396b65f19c8746c055f0b7c3f693e7"
+    "0xd2eb6976b405c2c25f0f849858e5f89629a29f0816d73dfd391d5c891a10468b" "0xbca937"
+    "0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb" "0x0000000000000000000000000000000000000000000000000000000000000000"
+    ["0x2ecd88a2c0ed09d136f175f9843f78c8532de065001dc135d58a9c30a71e933f",
+      "0x000000000000000000000000bbeb5e0adc0818ac8677b2f78387a8a2e22e8d16"]))
+
+subscriptionWithoutPohAddress :: EthSubscription
+subscriptionWithoutPohAddress = EthSubscription "2.0" "eth_subscription" (EthParams "0x1feed340b3559156f7e39013b6a1edd"
+    (EthResult False "0x134" "0x77" "0xc1014379a142881a1859f0faa7bd5ea5c9396b65f19c8746c055f0b7c3f693e7"
+    "0xd2eb6976b405c2c25f0f849858e5f89629a29f0816d73dfd391d5c891a10468b" "0xbca937"
+    "0x6B175474E89094C44Da98b954EedeAC495271d0F" "0x0000000000000000000000000000000000000000000000000000000000000000"
+    ["0x803727a67d35270dc2c090dc4f9cba1f9818a7200e65c2087eca187851fd6b19",
+     "0x000000000000000000000000bbeb5e0adc0818ac8677b2f78387a8a2e22e8d16"]))
+
+spec :: Spec
+spec = do
+  describe "Eth.Events.Dai tests" $ do
+    it "given poh addSubmission subscription when calling matches on addSubmission then return true" $ do
+      pohAddSubmission `shouldSatisfy` matches PohEvents.addSubmission
+    it "given poh subscription without addSubmission topic when calling matches on addSubmission then return false" $ do
+      subscriptionWithoutAddSubmissionTopic `shouldNotSatisfy` matches PohEvents.addSubmission
+    it "given subscription without poh contract address when calling matches on addSubmission then return false" $ do
+      subscriptionWithoutPohAddress `shouldNotSatisfy` matches PohEvents.addSubmission
+    it "given valid poh addSubmission subscription when calling asTweet on addSubmission then return expected text" $ do
+      expectedPohAddSubTweet <- T.readFile "test/Files/Eth/Events/ProofOfHumanity/expectedAddSubmissionTweet.txt"
+      asTweet PohEvents.addSubmission pohAddSubmission `shouldBe` expectedPohAddSubTweet
+
+main :: IO ()
+main = hspec spec
