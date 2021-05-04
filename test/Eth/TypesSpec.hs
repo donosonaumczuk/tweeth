0b13d20f9c61bcb8c1ec8fec88742dce69305c0d
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-04 03:27:55 -0300

    One more test case added

--- test/Eth/TypesSpec.hs
+++ test/Eth/TypesSpec.hs
@@ -5,6 +5,7 @@ module Eth.TypesSpec (main, spec) where
 import           Eth.Types
 import           Test.Hspec
 import qualified Data.Text.IO as T
+import Data.Aeson.Types (ToJSON)
 
 expectedSubscription :: EthSubscription
 expectedSubscription = EthSubscription "2.0" "eth_subscription" (EthParams "0x1feed340995274b5a3f497fb491dcb4e"
@@ -39,12 +40,18 @@ spec = do
     it "given subscription as json when calling fromJsonText then return just the expected EthSubscription" $ do
       jsonEthSubscription <- T.readFile "test/Files/Eth/Types/ethSubscription.json"
       fromJsonText jsonEthSubscription `shouldBe` Just expectedSubscription
+    it "given subscription when calling show then match the expected show output" $ do
+      showEthSubscription <- readFile "test/Files/Eth/Types/ethSubscription.txt"
+      show expectedSubscription `shouldBe` showEthSubscription
     it "given subscription that follows event matching condition when calling matches then return true" $ do
       expectedSubscription `shouldSatisfy` matches event
     it "given subscription when calling asTweet implemented as txHash then return the expected one" $ do
       asTweet event expectedSubscription `shouldBe` "0x2ecd88a2c0ed09d136f175f9843f78c8532de065001be03ed18bf9e4d550d553"
     it "given sub and list of matching events when calling findAndMapEventAsTweet then return just first asTweet" $ do
       findAndMapEventAsTweet [alwaysMatchingEvent, event] expectedSubscription `shouldBe` Just "Constant tweet!"
+    it "given sub and list of matching events when calling findAndMapEventAsTweet then return just first asTweet" $ do
+      let txHash = "0x2ecd88a2c0ed09d136f175f9843f78c8532de065001be03ed18bf9e4d550d553"
+      findAndMapEventAsTweet [neverMatchingEvent, event] expectedSubscription `shouldBe` Just txHash
     it "given sub and list of non matching events when calling findAndMapEventAsTweet then return nothing" $ do
       findAndMapEventAsTweet [neverMatchingEvent] expectedSubscription `shouldBe` Nothing
     it "given sub and empty list of events when calling findAndMapEventAsTweet then return nothing" $ do
